22c3c8b6a1ead3a692d804bf6e2ec10c0a6e82a8
Jesse <jrussellfreelance@gmail.com>
2020-09-19 15:15:39 -0400

    finished demo

--- demo-function/create-zip.sh
+++ demo-function/create-zip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export VIRTUALENV='venv_lambda'
 export ZIP_FILE='lambda.zip'
-export PYTHON_VERSION='python2.7'
+export PYTHON_VERSION='python3.8'
 # Zip dependencies from virtualenv, and main.py
 cd $VIRTUALENV/lib/$PYTHON_VERSION/site-packages/
 zip -r9 ../../../../$ZIP_FILE *
--- demo-function/main.py
+++ demo-function/main.py
@@ -1,3 +1,10 @@
-# file: main.py
+import json
+from whatsmyip.ip import get_ip
+from whatsmyip.providers import GoogleDnsProvider
+
 def handler(event, context):
-  plotly_url = event['plotly_url']
+    ipaddr = get_ip(GoogleDnsProvider)
+    return {
+        'statusCode': 200,
+        'body': json.dumps(ipaddr)
+    }
